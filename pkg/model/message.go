8895596b48dbcff5f26e841ef45766bf7863ce95
hijiki51 <hibiki0719euph@gmail.com>
2021-05-29 17:56:31 +0900

    convert struct

--- pkg/model/message.go
+++ pkg/model/message.go
@@ -2,13 +2,9 @@ package model
 
 import "time"
 
-type Message interface {
-	// メッセージの内容を取得します
-	GetContent() string
-	// メッセージ作成者のユーザーIDを取得します
-	GetUserID() string
-	// メッセージ作成者のユーザーネームを取得します
-	GetUserName() string
-	// メッセージの背区政日時を取得します
-	GetCreatedAt() time.Time
+type Message struct {
+	UserID    string
+	UserName  string
+	Text      string
+	CreatedAt time.Time
 }
