82fecdd5d5db7b180ff698edeb394f59bdeac5e9
hijiki51 <hibiki0719euph@gmail.com>
2021-05-29 18:27:37 +0900

    fix Generatereply

--- cmd/standard_reply/app/service.go
+++ cmd/standard_reply/app/service.go
@@ -10,12 +10,17 @@ import (
 type standerdReply struct {
 	MlService string
 }
+type replyRequest struct {
+	UserID   string `json:"user_id"`
+	UserName string `json:"user_name"`
+	Text     string `json:"text"`
+}
 
-func (s *standerdReply) GenerateReply(m model.Message) (model.Message, error) { //ひとまずプロパティは切り捨てる
+func (s *standerdReply) GenerateReply(m *model.Message) (*model.Message, error) { //ひとまずプロパティは切り捨てる
 	req := &replyRequest{
-		Message:  m.GetMessage(),
-		UserID:   m.GetUserID(),
-		UserName: m.GetUserName(),
+		UserID:   m.UserID,
+		UserName: m.UserName,
+		Text:     m.Text,
 	}
 	j, err := json.Marshal(req)
 	if err != nil {
@@ -25,11 +30,13 @@ func (s *standerdReply) GenerateReply(m model.Message) (model.Message, error) { 
 	if err != nil {
 		return nil, err
 	}
-	var message string
-	err = json.Unmarshal(b, &message)
+	var text string
+	err = json.Unmarshal(b, &text)
 	if err != nil {
 		return nil, err
 	}
-	m.SetMessage(message)
-	return m, nil
+	res := &model.Message{
+		Text: text,
+	}
+	return res, nil
 }
