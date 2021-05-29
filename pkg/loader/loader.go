521d168ed0ea6edc27776ea6a0362d0ee9949600
hijiki51 <hibiki0719euph@gmail.com>
2021-05-30 03:10:59 +0900

    add load Config

--- pkg/loader/loader.go
+++ pkg/loader/loader.go
@@ -1,5 +1,13 @@
 package loader
 
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+const configPath = "../../config/models.yaml"
+
 type model struct {
 	Name string `yaml:"name"`
 	Url  string `yaml:"url"`
@@ -12,4 +20,17 @@ type group struct {
 
 type Config struct {
 	Group group `yaml:"group,inline"`
+}
+
+func loadConfig() (*Config, error) {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	c := &Config{}
+	err = yaml.Unmarshal(data, c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
 }
