38607317e30a4a7aea130abee67b0e87ff4fa934
hijiki51 <hibiki0719euph@gmail.com>
2021-05-29 17:50:49 +0900

    add comment

--- pkg/model/message.go
+++ pkg/model/message.go
@@ -1,10 +1,14 @@
-package gateway
+package model
 
 import "time"
 
 type Message interface {
+	// メッセージの内容を取得します
 	GetContent() string
+	// メッセージ作成者のユーザーIDを取得します
 	GetUserID() string
+	// メッセージ作成者のユーザーネームを取得します
 	GetUserName() string
+	// メッセージの背区政日時を取得します
 	GetCreatedAt() time.Time
 }
