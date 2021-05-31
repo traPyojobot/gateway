f6d57b4823b72063683388d366934de277481320
hijiki51 <hibiki0719euph@gmail.com>
2021-05-31 12:39:48 +0900

    add query tag

--- pkg/model/message.go
+++ pkg/model/message.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Message struct {
-	UserID    string    `json:"user_id,omitempty"`
-	UserName  string    `json:"user_name,omitempty"`
-	Text      string    `json:"text,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
+	UserID    string    `json:"user_id,omitempty" query:"id"`
+	UserName  string    `json:"user_name,omitempty" query:"name"`
+	Text      string    `json:"text,omitempty" query:"text"`
+	CreatedAt time.Time `json:"created_at,omitempty" query:"created_at"`
 }
